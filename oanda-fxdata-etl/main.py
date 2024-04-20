5e445e74eff53f4fee7e8d789b4e47d1165eab77
Juraj Zachar <juraj.zachar@gmail.com>
2024-04-20 22:45:54 +0200

    lock thread assignment

--- oanda-fxdata-etl/main.py
+++ oanda-fxdata-etl/main.py
@@ -8,8 +8,13 @@ from db import Persistence
 from fs import list_in_dir, read_oanda_streams_file
 import threading
 
+persistence_pool = [Persistence.from_environment().connect() for x in range(PERSISTENCE_POOL_SIZE)]
 # Create a lock
 lock = threading.Lock()
+
+
+def assign_persistence():
+    return persistence_pool[randrange(PERSISTENCE_POOL_SIZE)]
 
 
 def process_file(entry):
@@ -17,13 +22,12 @@ def process_file(entry):
     logging.info(f"processing path={path}")
     try:
         for batch in list(partition(list(read_oanda_streams_file(path)), BATCH_SIZE)):
-            with lock:
-                persistence = assign_persistence()
-                persistence.insert_to_fx_prices(batch)
+            persistence = assign_persistence()
+            persistence.insert_to_fx_prices(batch)
     except Exception as e:
         logging.error(f"recovering from error {e.args}")
-    with lock:
-        marked = assign_persistence().mark_fx_file_processed(path)
+
+    marked = assign_persistence().mark_fx_file_processed(path)
     logging.info(f"path={marked} marked as successfully processed")
 
 
@@ -39,26 +43,19 @@ if __name__ == "__main__":
 
 
     try:
-        persistence_pool = [Persistence.from_environment().connect() for x in range(PERSISTENCE_POOL_SIZE)]
-
-
-        def assign_persistence():
-            return persistence_pool[randrange(PERSISTENCE_POOL_SIZE)]
-
-
         # phase 1: discover unprocessed files in directory
         for file in list_in_dir(FX_FOLDER):
             logging.info(f"saving unprocessed file path '{os.path.join(FX_FOLDER, file)}' to the database")
-            with lock:
-                assign_persistence().upsert_to_fx_files(folder=FX_FOLDER, filename=file)
+            assign_persistence().upsert_to_fx_files(folder=FX_FOLDER, filename=file)
 
         # phase 2: ingest into postgres timescaledb
         offset = 0
-        while unprocessed := persistence_pool[0].fetch_unprocessed(offset=offset, limit=5):
+        while unprocessed := assign_persistence().fetch_unprocessed(offset=offset, limit=12):
             logging.info(f"fetched unprocessed page [{offset}]={unprocessed}")
-            with ThreadPoolExecutor(max_workers=12) as executor:
-                executor.map(process_file, unprocessed)
-            offset += 1
+            with lock:
+                with ThreadPoolExecutor(max_workers=12) as executor:
+                    executor.map(process_file, unprocessed)
+                offset += 1
 
     except Exception as e:
         logging.error(f"failed to ingest and mark unprocessed oanda market data, reason:{e.args}")
