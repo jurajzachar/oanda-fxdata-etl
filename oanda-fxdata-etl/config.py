5fa3ddde963d0861a803e6f74227d646c66d82ad
Juraj Zachar <juraj.zachar@gmail.com>
2024-04-21 09:59:02 +0200

    remove unused config

--- oanda-fxdata-etl/config.py
+++ oanda-fxdata-etl/config.py
@@ -4,4 +4,3 @@ FX_FOLDER = os.environ.get('fx_folder', '/oxygen/oanda-streams')
 FX_LONG_TERM_STORAGE = os.environ.get('fx_long_store', '/hydrogen/oanda-streams')
 # fx_prices ingestion
 BATCH_SIZE = 100
-PERSISTENCE_POOL_SIZE = 20
